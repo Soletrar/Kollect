a84480c3f7d7dc6e42b5a802c4ce8ec1bbbc17b4
ric <cashdoacao@yahoo.com.br>
2022-10-14 07:40:20 -0300

    mudança coluna objeto para text

---
+++ database/migrations/2022_10_14_073630_edit_objeto_set_text.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::table('alteracoes', function (Blueprint $table) {
+            $table->text('objeto')->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('alteracoes', function (Blueprint $table) {
+            $table->string('objeto')->change();
+        });
+    }
+};
